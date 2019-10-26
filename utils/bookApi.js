8ec2d554330ce12314a0b27ae983bd25a0194b16
Kristina Jaggard <jaggardk@gmail.com>
2019-10-26 17:47:20 +0100

    hard code api call to google books

--- utils/bookApi.js
+++ utils/bookApi.js
@@ -1,8 +1,12 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// module.exports = async (keyword) => {
-//   const results = await axios({
-//     method: 'get',
-//     url: '',
-//   })
-// }
+module.exports = async keyword => {
+  try {
+    const results = await axios.get(
+      'https://www.googleapis.com/books/v1/volumes?q=power&printType=books&startIndex=0&maxResults=5&projection=lite'
+    );
+    console.log(results);
+  } catch (error) {
+    console.error(error);
+  }
+};
