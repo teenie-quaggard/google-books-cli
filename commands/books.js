bf433ca08ea94214a346d7f3b73968f8164fc74d
Kristina Jaggard <jaggardk@gmail.com>
2019-10-26 17:57:41 +0100

    create successful request to google books without keyword

--- commands/books.js
+++ commands/books.js
@@ -1,3 +1,19 @@
-module.exports = args => {
-  console.log('Looking for a book?');
+const ora = require('ora');
+const bookRequest = require('../utils/bookRequest.js');
+
+module.exports = async args => {
+  const spinner = ora().start();
+
+  try {
+    const keyword = args.keyword || args[1];
+    const books = await bookRequest(keyword);
+
+    spinner.stop();
+    console.log(
+      `Here are some books that have matched with your keyword: ${books}`
+    );
+  } catch (err) {
+    spinner.stop();
+    console.error(err);
+  }
 };
