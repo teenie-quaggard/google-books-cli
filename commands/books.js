b4d809f933409514725e083b38db1f6431e1b6a9
Kristina Jaggard <jaggardk@gmail.com>
2019-10-26 16:55:37 +0100

    create book and default commands

---
+++ commands/books.js
@@ -0,0 +1,3 @@
+module.exports = args => {
+  console.log('Looking for a book?');
+};
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const minimist = require('minimist');
+
 module.exports = () => {
-  console.log(`Hello world.`);
+  const args = minimist(process.argv.slice(2));
+  const command = args._[0];
+
+  switch (command) {
+    case 'books':
+      require('./commands/books')(args);
+      break;
+    default:
+      console.error(`Sorry, "${command}" is not a valid command.`);
+      break;
+  }
 };
