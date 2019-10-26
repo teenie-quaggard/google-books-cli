c081416bdff98c8ea063b3111e586272c10cf5fb
Kristina Jaggard <jaggardk@gmail.com>
2019-10-26 18:40:55 +0100

    successful call to Google Books API, returning 5 responses

--- commands/books.js
+++ commands/books.js
@@ -5,13 +5,26 @@ module.exports = async args => {
   const spinner = ora().start();
 
   try {
-    const keyword = args.keyword || args[1];
+    const keyword = args.keyword || args._1;
     const books = await bookRequest(keyword);
-
     spinner.stop();
-    console.log(
-      `Here are some books that have matched with your keyword: ${books}`
-    );
+
+    /**********************RESPONSE**************************/
+    console.log(`You searched for "${keyword}".`);
+    books.forEach(book => {
+      const info = book.volumeInfo;
+
+      console.log(`
+      Title: ${info.title}
+      Author(s): ${info.author}
+      Publisher: ${info.publisher}
+      Date published: ${info.publishedDate}
+      Description: ${info.description}
+
+      _____________________________________________
+
+      `);
+    });
   } catch (err) {
     spinner.stop();
     console.error(err);
--- utils/bookRequest.js
+++ utils/bookRequest.js
@@ -3,11 +3,12 @@ const axios = require('axios');
 const key = process.env.GOOGLE_BOOKS_API_KEY;
 
 module.exports = async keyword => {
+  await new Promise(resolve => setTimeout(resolve, 1000));
   try {
     const results = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=power&printType=books&startIndex=0&maxResults=5&projection=lite&key=${key}`
+      `https://www.googleapis.com/books/v1/volumes?q=${keyword}&printType=books&startIndex=0&maxResults=5&projection=lite&key=${key}`
     );
-    console.log(results.data.items);
+    return results.data.items;
   } catch (error) {
     console.error(error);
   }
