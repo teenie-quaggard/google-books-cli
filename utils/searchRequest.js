7093c2693029d5e45715ad9d44da3af54fd993b2
Kristina Jaggard <jaggardk@gmail.com>
2019-10-27 23:16:43 +0000

    remove Google Books API key as it is not necessary for search requests. Remove dotenv from package.json

--- utils/searchRequest.js
+++ utils/searchRequest.js
@@ -1,12 +1,10 @@
 const axios = require('axios');
 
-const key = process.env.GOOGLE_BOOKS_API_KEY;
-
 module.exports = async keyword => {
   await new Promise(resolve => setTimeout(resolve, 1000));
   try {
     const results = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=${keyword}&printType=books&startIndex=0&maxResults=5&projection=lite&key=${key}`
+      `https://www.googleapis.com/books/v1/volumes?q=${keyword}&printType=books&startIndex=0&maxResults=5&projection=lite`
     );
     return results.data.items;
   } catch (error) {
