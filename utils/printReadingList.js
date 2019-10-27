c2fc9ed787956c52db4926d6bfa5b22d3546b2ea
Kristina Jaggard <jaggardk@gmail.com>
2019-10-27 23:13:08 +0000

    fix typos

--- utils/printReadingList.js
+++ utils/printReadingList.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = () => {
   fs.readFile('reading-list.txt', 'utf8', (err, list) => {
     if (err) {
-      console.error(`There's been an issue finding your list!\nerr`);
+      console.error(`There's been an issue finding your list!\n${err}`);
     }
     console.log(
       `Your reading list:\n___________________________________\n\n${list}`
