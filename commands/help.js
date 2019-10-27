4ec675870cb212e74a5f0d92dd3ed4f366ec854e
Kristina Jaggard <jaggardk@gmail.com>
2019-10-27 20:35:45 +0000

    fix typos in help menu and add clarifying instructions on what commands need to be paired with a flag

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,9 @@
 const menus = {
   main: `
-    search [command] <options>
+    books-cli [command] <options>
 
-    search......................search Google API for a book using a keyword. Should be paired with the --keyword flag
-    save........................save a book to a reading list using the book ID. Should be paired with the --id flag
+    search......................search Google API for a book using a keyword. Should be paired with the --keyword flag and a search phrase.
+    save........................save a book to a reading list. Should be paired with the --id flag and the ID of a book you'd like to save.
     version.....................show package version
     help........................show help menu for a command`,
 
@@ -13,7 +13,7 @@ const menus = {
 
   save: `
     books-cli save <options>
-    --id, -i ..... the id of the book you'd like to save to your reading list`
+    --id, -i ..... the ID of the book you'd like to save to your reading list`
 };
 module.exports = args => {
   const subCmd = args._[0] === 'help' ? args._[1] : args._[0];
