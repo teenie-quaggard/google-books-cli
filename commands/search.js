827bf467657330cce5f24caa3adba8f76eeb5ec0
Kristina Jaggard <jaggardk@gmail.com>
2019-10-27 21:24:46 +0000

    include console.error if user does not enter a keyword

--- commands/search.js
+++ commands/search.js
@@ -9,28 +9,46 @@ module.exports = async args => {
     const books = await bookRequest(keyword);
     spinner.stop();
 
-    /**********************RESPONSE**************************/
-    console.log(
-      `\n
+    if (keyword === undefined) {
+      console.error(`
       _____________________________________________________________
-      \n
-      You searched for: "${keyword}". \n
-      Here are five books related to your search. If you are interested in saving one to your reading list \n
       
+      Your search phrase returned undefined.  
+      Please ensure that you have used the --keyword flag and have followed it with search term.
+      Queries of more than one word should be wrapped in quotation marks.
+
+      Your query should follow one of the following structures: 
+      
+      books-cli search --keyword word
+      books-cli search --keyword "search phrase"
+      
+      Please try searching again.
       _____________________________________________________________
-      `
-    );
-    books.forEach(book => {
-      console.log(`
-      Title: ${book.volumeInfo.title}
-      Author(s): ${book.volumeInfo.authors}
-      Publisher: ${book.volumeInfo.publisher}
-
-      ID: ${book.id}
-
-      ____________________________________________
       `);
-    });
+    } else {
+      /**********************RESPONSE**************************/
+      console.log(
+        `\n
+        _____________________________________________________________
+        \n
+        You searched for: "${keyword}". \n
+        Here are five books related to your search. If you are interested in saving one to your reading list \n
+        
+        _____________________________________________________________
+        `
+      );
+      books.forEach(book => {
+        console.log(`
+        Title: ${book.volumeInfo.title}
+        Author(s): ${book.volumeInfo.authors}
+        Publisher: ${book.volumeInfo.publisher}
+
+        ID: ${book.id}
+
+        ____________________________________________
+        `);
+      });
+    }
   } catch (err) {
     spinner.stop();
     console.error(err);
