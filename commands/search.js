af60580e6f1a7e4bee5fbf3ae30cb79605a3c4e4
Kristina Jaggard <jaggardk@gmail.com>
2019-10-29 21:15:57 +0000

    update copy on prompt to save a book after searching

--- commands/search.js
+++ commands/search.js
@@ -19,7 +19,7 @@ module.exports = async args => {
 
       Your query should follow one of the following structures: 
       
-      books-cli search --keyword word
+      books-cli search --keyword word \n
       books-cli search --keyword "search phrase"
       
       Please try searching again.
@@ -32,7 +32,12 @@ module.exports = async args => {
         _____________________________________________________________
         \n
         You searched for: "${keyword}". \n
-        Here are five books related to your search. If you are interested in saving one to your reading list \n
+        Here are five books related to your search. \n \n \n
+        If you are interested in saving a book to your reading list, \n
+        copy its listed ID and use the following command to save that book to your reading list. \n
+        Remember that you need to replace the last word in the command with the ID of the book you'd like to save. \n \n
+
+        books-cli save --id replaceThisWithYourBookID
         
         _____________________________________________________________
         `
