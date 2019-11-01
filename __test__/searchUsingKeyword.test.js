19a28b3c23282f3242c86679d6afec4eae48d867
Kristina Jaggard <jaggardk@gmail.com>
2019-11-01 16:36:27 +0000

    remove broken fs mocks

--- __test__/searchUsingKeyword.test.js
+++ __test__/searchUsingKeyword.test.js
@@ -1,13 +1,15 @@
 const mockAxios = require('axios');
 const searchUsingKeyword = require('../utils/searchUsingKeyword');
-const dummyResponse = require('../__fixtures__/dummyKeywordResponse');
+const dummyKeywordResponse = require('../__fixtures__/dummyKeywordResponse');
 
 test('it calls Google Books API and returns books', async () => {
-  mockAxios.get.mockImplementationOnce(() => Promise.resolve(dummyResponse));
+  mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve(dummyKeywordResponse)
+  );
   const keyword = 'dance';
   const books = await searchUsingKeyword(keyword);
 
-  expect(books).toEqual(dummyResponse);
+  expect(books).toEqual(dummyKeywordResponse);
   expect(mockAxios.get).toHaveBeenCalledTimes(1);
   expect(mockAxios.get).toHaveBeenCalledWith(
     'https://www.googleapis.com/books/v1/volumes?q=dance&printType=books&startIndex=0&maxResults=5&projection=lite'
