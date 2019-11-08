2b15c519933aff96539617cf217ebc90aff9b890
Kristina Jaggard <jaggardk@gmail.com>
2019-11-08 23:57:14 +0000

    add condition for when reading list is empty

--- utils/printReadingList.js
+++ utils/printReadingList.js
@@ -4,9 +4,12 @@ module.exports = () => {
   fs.readFile('reading-list.txt', 'utf8', (err, list) => {
     if (err) {
       console.error(`There's been an issue finding your list!\n${err}`);
+    } else if (!list) {
+      console.log(`Your reading list is empty.`);
+    } else {
+      console.log(
+        `Your reading list:\n___________________________________\n\n${list}`
+      );
     }
-    console.log(
-      `Your reading list:\n___________________________________\n\n${list}`
-    );
   });
 };
