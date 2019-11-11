a14e340c77bbd3dc9741a3c60c585652b630013d
Kristina Jaggard <jaggardk@gmail.com>
2019-11-11 15:50:40 +0000

    add error handling in searchUsingId function. test error handling

---
+++ __tests__/searchUsingId.test.js
@@ -0,0 +1,31 @@
+const mockAxios = require('axios');
+const searchUsingId = require('../utils/searchUsingId');
+const dummyIdResponse = require('../__fixtures__/dummyIdResponse');
+
+describe('searchUsingId', () => {
+  it('calls Google Books API using an ID and returns one book', async () => {
+    mockAxios.get.mockImplementationOnce(() =>
+      Promise.resolve(dummyIdResponse)
+    );
+    const id = 'bUcdDgAAQBAJ';
+    const book = await searchUsingId(id);
+
+    expect(book).toEqual(dummyIdResponse);
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      'https://www.googleapis.com/books/v1/volumes/bUcdDgAAQBAJ'
+    );
+  });
+
+  it('throws an error if book ID is not passed', async () => {
+    const error = await searchUsingId();
+    expect(error.name).toBe('Error');
+    expect(error.message).toBe('Incorrect book ID input');
+  });
+
+  it('throws an error if book ID is not a string', async () => {
+    const error = await searchUsingId(true);
+    expect(error.name).toBe('Error');
+    expect(error.message).toBe('Incorrect book ID input');
+  });
+});
--- utils/searchUsingId.js
+++ utils/searchUsingId.js
@@ -4,11 +4,20 @@ const flashSpinner = require('./flashSpinner');
 module.exports = async id => {
   await flashSpinner();
   try {
+    if (!id || typeof id !== 'string') {
+      throw new Error('Incorrect book ID input');
+    }
     const results = await axios.get(
       `https://www.googleapis.com/books/v1/volumes/${id}`
     );
     return results;
   } catch (error) {
+    if (error.message === 'Request failed with status code 503') {
+      throw new Error(
+        "Sorry, there's been a problem. Most likely, you have used a book ID that could not be found on the Google Books database. Check that your ID is correct and try again."
+      );
+    }
     console.error(error);
+    return error;
   }
 };
